a05db9641355bfebf590c0be2082b4b671b38cb2
Arthur <aking4511@gmail.com>
2023-05-05 13:16:53 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ usePassport(app)
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routes) //router放最後
