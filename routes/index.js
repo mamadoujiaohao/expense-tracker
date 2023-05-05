21d831d949c8d7a9cdc8a5a14901e014792ff402
Arthur <aking4511@gmail.com>
2023-05-05 11:54:33 +0800

    feat: add user registration feature

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const expense = require('./modules/expense')
 const add = require('./modules/add')
-const user = require('/module/user')
+const user = require('./modules/user')
 
 router.use('/', home)
 router.use('/expense', expense)
-router.use('/add', add)
+//router.use('/add', add)
 router.use('/user', user)
 
 module.exports = router
