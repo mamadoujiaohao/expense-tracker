cd8ccf89856bb5b2924f0e9c9ca4601c2c2415da
Arthur <aking4511@gmail.com>
2023-05-05 11:57:27 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-const db = require('./config/mongoose')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const routes = require('./routes')//express自動找index.js
+require('./config/mongoose')
 
 if (process.env.NODE_ENV !== 'production') { require('dotenv').config()} 
 const port = process.env.PORT
 
-const routes = require('./routes')//express自動找index.js
-require('./config/mongoose')
+
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' })) //版本7.0.7需要用exphbs.engine(),否則會報錯
 app.set('view engine', 'hbs')
 
+app.use(session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+  }))
 app.use(routes)
 
 app.listen(port, ()=>{
