3fa0ae47a6430334437f0c0093ae1d3de8a01e5d
aking4511 <aking4511@gmail.com>
2023-05-13 15:48:25 +0800

    feat: editing filter(not finished yet)

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,25 +3,6 @@ const router = express.Router()
 const Expenses = require('../../models/expense')
 const Categories = require('../../models/category')
 
-// router.get('/sort', (req, res) => {
-//   const userId = req.user._id
-//   const selectedCategoryId = Number(req.query.sort)
-//   if (selectedCategoryId === 6) {
-//     return res.redirect('/')
-//   }
-//   Expenses.find({ userId, categoryId: selectedCategoryId })
-//     .lean()
-//     .then(expenses => {
-//       let totalAmount = 0
-//       expenses.forEach(expense => {
-//         totalAmount += expense.amount
-//         expense.date = expense.date.toISOString().split('T')[0].replace(/-/g, '/')
-//       })
-//       console.log(selectedCategoryId)
-//       res.render('index',{ expenses, totalAmount, selectedCategoryId })
-//     })
-//     .catch(err => console.log(err))
-// })
 
 router.get('/', (req, res) => {
   const userId = req.user._id
@@ -34,11 +15,13 @@ router.get('/', (req, res) => {
       expenses.forEach(expense => {
         totalAmount += expense.amount
         expense.date = new Date(expense.date).toISOString().split('T')[0].replace(/-/g, '/')
-        return Categories.find({}).sort({ date: 'desc' })
       })
-      res.render('home',{ expenses, totalAmount })
+      res.render('home',{ expenses, totalAmount, categories })
     })
     .catch(err => console.log(err))
 })
+
+
+
 
 module.exports = router
